0534de0078f1834bc1e3ca5799ba47eea2d2f02b
jbwheatley <jackbwheatley@gmail.com>
2019-08-16 12:50:53 +0100

    DecodeAndMigrate effectful threading

--- core/src/main/scala/io/github/mcgizzle/DecodeAndMigrate.scala
+++ core/src/main/scala/io/github/mcgizzle/DecodeAndMigrate.scala
@@ -1,49 +1,53 @@
 package io.github.mcgizzle
 
-import cats.Id
+import cats.{Applicative, Id, Monad}
 import cats.implicits._
 import shapeless.{Lazy, Nat, Succ}
 
-final class DecodeAndMigrate[Origin] {
-  def from[A, Start <: Nat, Target <: Nat](a: A)(implicit ev: DecodeAndMigrateBuilder[Origin, A, Start, Target]): Option[ev.Out] = ev.decodeAndMigrate(a)
+final class DecodeAndMigrate[F[_], Origin] {
+  def from[A, Start <: Nat, Target <: Nat](a: A)(implicit ev: DecodeAndMigrateBuilder[F, Origin, A, Start, Target]): F[Option[ev.Out]] = ev.decodeAndMigrate(a)
 }
 
 object DecodeAndMigrate {
-  def apply[Origin]: DecodeAndMigrate[Origin] = new DecodeAndMigrate[Origin]
+  def apply[Origin]: DecodeAndMigrate[Id, Origin] = new DecodeAndMigrate[Id, Origin]
 }
 
-sealed trait DecodeAndMigrateBuilder[Origin, A, Start <: Nat, Target <: Nat] {
+sealed trait DecodeAndMigrateBuilder[F[_], Origin, A, Start <: Nat, Target <: Nat] {
 
   type Out
 
-  def decodeAndMigrate(a: A): Option[Out]
+  def decodeAndMigrate(a: A): F[Option[Out]]
 
 }
 
 object DecodeAndMigrateBuilder  {
 
-  type Aux[Origin, A, Start <: Nat, Target <: Nat, Out0] = DecodeAndMigrateBuilder[Origin, A, Start, Target] { type Out = Out0 }
+  type Aux[F[_], Origin, A, Start <: Nat, Target <: Nat, Out0] = DecodeAndMigrateBuilder[F, Origin, A, Start, Target] { type Out = Out0 }
 
-  implicit def recurse[Origin, A, N <: Nat, Target <: Nat, D, DTarget]
+  implicit def recurse[F[_]: Monad, Origin, A, N <: Nat, Target <: Nat, D, DTarget]
   (implicit
    v: Versioned.Aux[Origin, N, D],
    ev: Decoder[A, D],
-   m: MigrationBuilder.Aux[Id, Origin, N, Target, D, DTarget],
-   r: Lazy[DecodeAndMigrateBuilder.Aux[Origin, A, Succ[N], Target, DTarget]],
-  ): DecodeAndMigrateBuilder.Aux[Origin, A, N, Target, DTarget] =
-    new DecodeAndMigrateBuilder[Origin, A, N, Target] {
+   m: MigrationBuilder.Aux[F, Origin, N, Target, D, DTarget],
+   r: Lazy[DecodeAndMigrateBuilder.Aux[F, Origin, A, Succ[N], Target, DTarget]]
+  ): DecodeAndMigrateBuilder.Aux[F, Origin, A, N, Target, DTarget] =
+    new DecodeAndMigrateBuilder[F, Origin, A, N, Target] {
       type Out = DTarget
-      def decodeAndMigrate(a: A): Option[DTarget] = r.value.decodeAndMigrate(a) <+> ev.decode(a).map(m.migrate)
+      def decodeAndMigrate(a: A): F[Option[DTarget]] =
+        for {
+          x <- r.value.decodeAndMigrate(a)
+          y <- ev.decode(a).map(m.migrate).sequence
+        } yield x <+> y
     }
 
-  implicit def base[Origin, A, N <: Nat, DN]
+  implicit def base[F[_]:Applicative, Origin, A, N <: Nat, DN]
   (implicit
    v: Versioned.Aux[Origin, N, DN],
    d: Decoder[A, DN]
-  ): DecodeAndMigrateBuilder.Aux[Origin, A, N, N, DN] =
-    new DecodeAndMigrateBuilder[Origin, A, N, N] {
+  ): DecodeAndMigrateBuilder.Aux[F, Origin, A, N, N, DN] =
+    new DecodeAndMigrateBuilder[F, Origin, A, N, N] {
       type Out = DN
-      def decodeAndMigrate(a: A): Option[DN] = d.decode(a)
+      def decodeAndMigrate(a: A): F[Option[DN]] = Applicative[F].pure(d.decode(a))
   }
 }
 
