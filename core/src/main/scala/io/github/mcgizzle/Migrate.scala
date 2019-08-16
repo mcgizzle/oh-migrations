ab09ace8764473188ecf2dc49737403593654d40
jbwheatley <jackbwheatley@gmail.com>
2019-08-16 12:19:09 +0100

    add effect to Migrate; change MigrationFunction to a Kleisli; package object

--- core/src/main/scala/io/github/mcgizzle/Migrate.scala
+++ core/src/main/scala/io/github/mcgizzle/Migrate.scala
@@ -1,54 +1,58 @@
 package io.github.mcgizzle
 
+import cats.{FlatMap, Id}
+import cats.data.Kleisli
 import shapeless.{Nat, Succ}
 
-class Migrate[Origin] {
+class Migrate[F[_], Origin] {
 
-  def from[Start <: Nat, End <: Nat](implicit m: MigrationBuilder[Origin, Start, End]): m.Data1 => m.Data2 = m.migrate(_)
+  def from[Start <: Nat, End <: Nat](implicit m: MigrationBuilder[F, Origin, Start, End]): m.Data1 => F[m.Data2] = m.migrate(_)
 
 }
 
 object Migrate {
-  def apply[Origin]: Migrate[Origin] = new Migrate[Origin]
+  def apply[Origin]: Migrate[Id, Origin] = new Migrate[Id, Origin]
 }
 
-trait MigrationBuilder[Origin, Start <: Nat, End <: Nat] {
+trait MigrationBuilder[F[_], Origin, Start <: Nat, End <: Nat] {
 
   type Data1
   type Data2
 
-  def migrate(d: Data1): Data2
+  def migrate(d: Data1): F[Data2]
 }
 
 object MigrationBuilder {
 
-  type Aux[Origin, Start <: Nat, End <: Nat, D1, D2] = MigrationBuilder[Origin, Start, End] {
+  import cats.syntax.flatMap._
+
+  type Aux[F[_], Origin, Start <: Nat, End <: Nat, D1, D2] = MigrationBuilder[F, Origin, Start, End] {
     type Data1 = D1
     type Data2 = D2
   }
 
-  implicit def recurse[Origin, Start <: Nat, N <: Nat, DStart, DN, DEnd]
+  implicit def recurse[F[_]: FlatMap, Origin, Start <: Nat, N <: Nat, DStart, DN, DEnd]
   (implicit
    v1: Versioned.Aux[Origin, Start, DStart],
    v2: Versioned.Aux[Origin, Succ[N], DEnd],
    v3: Versioned.Aux[Origin, N, DN],
-   f: MigrationFunction[DN, DEnd],
-   r: MigrationBuilder.Aux[Origin, Start, N, DStart, DN],
-  ): MigrationBuilder.Aux[Origin, Start, Succ[N], DStart, DEnd] = new MigrationBuilder[Origin, Start, Succ[N]] {
+   f: Kleisli[F, DN, DEnd],
+   r: MigrationBuilder.Aux[F, Origin, Start, N, DStart, DN]
+  ): MigrationBuilder.Aux[F, Origin, Start, Succ[N], DStart, DEnd] = new MigrationBuilder[F, Origin, Start, Succ[N]] {
     type Data1 = DStart
     type Data2 = DEnd
 
-    def migrate(d: DStart): DEnd = f(r.migrate(d))
+    def migrate(d: DStart): F[DEnd] = r.migrate(d).flatMap(f.run)
   }
 
-  implicit def base[Origin, V <: Nat, D, DNext]
+  implicit def base[F[_], Origin, V <: Nat, D, DNext]
   (implicit
    v: Versioned.Aux[Origin, V, D],
    v1: Versioned.Aux[Origin, Succ[V], DNext],
-   f: MigrationFunction[D, DNext]): MigrationBuilder.Aux[Origin, V, Succ[V], D, DNext] = new MigrationBuilder[Origin, V, Succ[V]] {
+   f: Kleisli[F, D, DNext]): MigrationBuilder.Aux[F, Origin, V, Succ[V], D, DNext] = new MigrationBuilder[F, Origin, V, Succ[V]] {
     type Data1 = D
     type Data2 = DNext
-    def migrate(d: D): DNext = f.apply(d)
+    def migrate(d: D): F[DNext] = f(d)
   }
 }
 
--- core/src/main/scala/io/github/mcgizzle/MigrationFunction.scala
+++ core/src/main/scala/io/github/mcgizzle/MigrationFunction.scala
@@ -1,16 +1,8 @@
 package io.github.mcgizzle
 
-trait MigrationFunction[Data1, Data2] {
-  def apply(d: Data1): Data2
-}
-
+import cats.Id
+import cats.data.Kleisli
 
 object MigrationFunction {
-
-  type +=>[Data1, Data2] = MigrationFunction[Data1, Data2]
-
-  def apply[Data1, Data2](f: Data1 => Data2): MigrationFunction[Data1, Data2] = new MigrationFunction[Data1, Data2] {
-    def apply(d: Data1): Data2 = f(d)
-  }
-
+  def apply[Data1, Data2](f: Data1 => Data2): Data1 +=> Data2 = Kleisli[Id, Data1, Data2](f)
 }
