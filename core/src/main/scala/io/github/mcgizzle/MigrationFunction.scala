2c72d0985f48b151326b154285e23f3add74c421
McGizzle <mcgroas@tcd.ie>
2019-08-16 09:58:04 +0100

    infix type operator for MigrationFunction

--- core/src/main/scala/io/github/mcgizzle/MigrationFunction.scala
+++ core/src/main/scala/io/github/mcgizzle/MigrationFunction.scala
@@ -4,7 +4,10 @@ trait MigrationFunction[Data1, Data2] {
   def apply(d: Data1): Data2
 }
 
+
 object MigrationFunction {
+
+  type +=>[Data1, Data2] = MigrationFunction[Data1, Data2]
 
   def apply[Data1, Data2](f: Data1 => Data2): MigrationFunction[Data1, Data2] = new MigrationFunction[Data1, Data2] {
     def apply(d: Data1): Data2 = f(d)
